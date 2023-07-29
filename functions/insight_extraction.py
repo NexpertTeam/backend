99256c417691bf0483ced3a792cebecfa1717bd8
santiag0m <32421689+santiag0m@users.noreply.github.com>
2023-07-29 15:17:26 -0700

    Update key insights extraction

--- functions/insight_extraction.py
+++ functions/insight_extraction.py
@@ -1,37 +1,43 @@
 from claude import Claude
 
+from anthropic import HUMAN_PROMPT, AI_PROMPT
+
 
 def extract_key_insights(paper_text: str) -> list[dict]:
-    insight_extractor = Claude()
-    insights = insight_extractor(
-        f"""List the main ideas of the paper that reference previous work, for each idea add the APA format related references in the format:
-
-        <idea> <description>
-
-        # Description of the idea
-
-        </description> <references> <bibitem>
-
-        <bibkey>#Bibtext friendly ID</bibkey>
-
-        <reference_text>#APA formatted reference</reference_text>
-        </bibitem>
-
-        ...
+    prompt = f"""List the most important ideas in the paper that build
+    upon previous work (avoid referencing the paper itself) and then output the list of ideas in the following format:
+    <references>
         <bibitem>
+            <bibkey>
+                # Bibtext friendly ID
+            </bibkey>
+            <reference_text>
+                # APA formatted reference
+            </reference_text>
+        </bibitem>
+    </references>
+    <idea>
+        <description>
+            # Description of the idea
+        </description>
+        <relevant_references>
+            <bibkey>
+                # Bibkey ID of relevant reference
+            </bibkey>
+            ...
+            <bibkey>
+            ...
+            </bibkey>
+        </relevant_references>
+    </idea>
+    ...
+    <idea>
+    ...
+    </idea>
+    === Paper text ===
+    {paper_text}
 
-        ...
-
-        </bibitem> </references> </idea>
-
-        ...
-        <idea>
-
-        ...
-        </idea>)
-
-        === Paper text ===
-        {paper_text}
-        """
-    )
+    {AI_PROMPT} I have identified the paper's title and authors and will ignore it, beyond that, these are the most important references from previous work:
+    """
+    insights = Claude()(prompt, output_role_or_suffix="")
     return insights
