213d0518d1b1699570f4c9b4d1b1c696ff0ea339
santiag0m <32421689+santiag0m@users.noreply.github.com>
2023-07-29 13:08:01 -0700

    First commit

---
+++ pdf_parser.py
@@ -0,0 +1,9 @@
+from pypdf import PdfReader
+
+
+def pdf_to_text(path_to_pdf: str) -> str:
+    reader = PdfReader(path_to_pdf)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
