eb0e5390df04f6c5f59995d70c09254fd38015de
santiag0m <32421689+santiag0m@users.noreply.github.com>
2023-07-29 15:29:55 -0700

    Add parser

---
+++ xml_parser.py
@@ -0,0 +1,23 @@
+import re
+
+
+def extract_tag_content(input_string, tag):
+    # Define the pattern to match the specified tag
+    pattern = f"<{tag}>(.*?)</{tag}>"
+
+    # Find all occurrences of the pattern and return them as a list
+    return re.findall(pattern, input_string)
+
+
+# from lxml import etree
+
+
+# def extract_tag_content(xml_string, tag):
+#     # Parse the XML string
+#     root = etree.fromstring(xml_string)
+
+#     # Find all occurrences of the specified tag
+#     elements = root.findall(f".//{tag}")
+
+#     # Extract the text content of each element and return it as a list
+#     return [element.text for element in elements]
