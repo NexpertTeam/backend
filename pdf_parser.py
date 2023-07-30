6e301ddd13e0f4f18431c4357098c2e387e0a178
santiag0m <32421689+santiag0m@users.noreply.github.com>
2023-07-29 21:10:12 -0700

    Add caches

--- pdf_parser.py
+++ pdf_parser.py
@@ -6,18 +6,21 @@ from functools import lru_cache
 from PyPDF2 import PdfReader
 
 
+@lru_cache(maxsize=30)
 def pdf_url_to_text(url: str) -> str:
     tempfile = download_pdf(url)
     text = pdf_to_text(tempfile)
     return text
 
 
+@lru_cache(maxsize=30)
 def pdf_to_text(path_to_pdf: str) -> str:
     reader = PdfReader(path_to_pdf)
     text = ""
     for page in reader.pages:
         text += page.extract_text()
     return text
+
 
 @lru_cache(maxsize=30)
 def download_pdf(url):
@@ -33,12 +36,11 @@ def download_pdf(url):
     temp_dir = tempfile.mkdtemp()
 
     # Determine the file name from the URL
-    filename = os.path.join(temp_dir, url.split('/')[-1])
+    filename = os.path.join(temp_dir, url.split("/")[-1])
 
     # Write the content to a PDF file
-    with open(filename, 'wb') as file:
+    with open(filename, "wb") as file:
         file.write(response.content)
 
     print(f"Downloaded file saved in {filename}")
     return filename
-
